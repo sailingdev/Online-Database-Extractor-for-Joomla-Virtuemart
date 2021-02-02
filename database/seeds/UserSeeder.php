39ecbc3a3d7480217f97864b92840d3e5884984c
sailingdev <sailingdev123@gmail.com>
2021-02-03 00:40:33 +0800

    UserSeeder configuration

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'manage user']);
+        Permission::create(['name' => 'manage role_permission']);
+        Permission::create(['name' => 'extract db']);
+
+        // create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'admin']);
+        $role1->givePermissionTo('manage user');
+        $role1->givePermissionTo('manage role_permission');
+        $role1->givePermissionTo('extract db');
+
+        $role2 = Role::create(['name' => 'user']);
+        $role2->givePermissionTo('extract db');
+
+        // crate super-admin and assign existing role
+        $user = User::create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin123!@#')
+        ]);
+        $user->assignRole($role1);
+    }
+}
