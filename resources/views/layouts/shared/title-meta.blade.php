bf43e4888345360f3fcb357ed9e8c5e3efacec1d
sailingdev <sailingdev123@gmail.com>
2021-03-03 08:33:42 +0800

    modify title-meta.blade.php

--- resources/views/layouts/shared/title-meta.blade.php
+++ resources/views/layouts/shared/title-meta.blade.php
@@ -4,5 +4,6 @@
 <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
 <meta content="Coderthemes" name="author" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 <!-- App favicon -->
 <link rel="shortcut icon" href="{{asset('assets/images/favicon.ico')}}">
